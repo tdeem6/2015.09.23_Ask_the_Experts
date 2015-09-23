a9eb6d867c25403ce68aeee6f991296bd658a1c1
Tyler Deem <tdeem6@gmail.com>
2015-09-23 15:30:58 -0700

    adds QuestionsController.js

---
+++ controllers/QuestionsController.js
@@ -0,0 +1,4 @@
+expertSite.controller('QuestionsCtrl', function QuestionsCtrl($scope, QuestionsFactory) {
+  $scope.questions = QuestionsFactory.questions;
+  $scope.QuestionsFactory = QuestionsFactory;
+});
