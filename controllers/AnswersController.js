de80d62bd2870f7056fa3d168acd6f6e65fcd888
Tyler Deem <tdeem6@gmail.com>
2015-09-23 15:36:35 -0700

    adds AnswersController.js

---
+++ controllers/AnswersController.js
@@ -0,0 +1,7 @@
+expertSite.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionsFactory, UtilitiesFactory) {
+  $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
+  $scope.addAnswer = function() {
+    $scope.question.answers.push({ reply: $scope.answerReply });
+    $scope.answerReply = null;
+  }
+});
